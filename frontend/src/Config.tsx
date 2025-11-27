0b86cecd815b74acee0226c8b198092fd90a12c1
domke <673751-domke@users.noreply.gitlab.com>
2025-11-27 17:41:35 +0900

    new links

--- frontend/src/Config.tsx
+++ frontend/src/Config.tsx
@@ -1,3 +1,3 @@
-export const personallyIdentifiablePolicyLink = "https://trip.riken.jp/en/aijam/index.html" //"https://my.anl.gov/article/protecting-personally-identifiable-information-pii";
-export const confidentialInformationPolicyLink = "https://trip.riken.jp/en/aijam/index.html" //"https://www.energy.gov/sites/default/files/2024-08/science-technology-risk-matrix-unclassified.pdf";
-//export const exportControlPolicyLink = "https://trip.riken.jp/en/aijam/index.html" //"https://anl.app.box.com/s/g0bbjqeyrxfrgezsrzpbxpwbvkhlpf0z";
+export const personallyIdentifiablePolicyLink = "https://rbs-common.riken.jp/contents/rikenbbs/detail/181116-01-e.pdf";
+export const confidentialInformationPolicyLink = "https://rbs-i-intra.riken.jp/en/security/implementation/";
+export const exportControlPolicyLink = "https://rbs-common-intra.riken.jp/riesd/en/02/v2/000.html";
