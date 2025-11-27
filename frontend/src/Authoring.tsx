76193e36d8360292394c6ef6157775f2e43ecbcf
domke <673751-domke@users.noreply.gitlab.com>
2025-11-27 21:25:13 +0900

    lets see

--- frontend/src/Authoring.tsx
+++ frontend/src/Authoring.tsx
@@ -184,15 +184,15 @@ interface QuestionsFormProps {
 
 export function QuestionsForm({ author }: QuestionsFormProps) {
   let isAuthenticated: boolean;
-  let auth_token: string|undefined;
-  if(import.meta.env.VITE_USE_GLOBUS == "true") {
+  let auth_token: string|import.meta.env.QUESTIONSUI_AI_API_KEY;
+  /*if(import.meta.env.VITE_USE_GLOBUS == "true") {
       const { authorization,  isAuthenticated: isGlobusAuthenticated} = useGlobusAuth();
       isAuthenticated = isGlobusAuthenticated;
       auth_token = authorization?.tokens?.getByResourceServer("681c10cc-f684-4540-bcd7-0b4df3bc26ef")?.access_token;
   } else {
-      auth_token = import.meta.env.QUESTIONSUI_AI_API_KEY; //"disabled";
+      auth_token = "disabled";
       isAuthenticated = true;
-  }
+  }*/
 
   const [edited, setEdited] = useState(false);
   const [question, setQuestionImpl] = useState("");
